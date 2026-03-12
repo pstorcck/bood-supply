9588d537a63444c347a316777c9d2515e8d3d28b
Paul Storck <paul@MacBook-Pro-de-Paul.local>
2026-03-12 17:11:11 -0600

    Fix middleware matcher

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,11 @@
 import createMiddleware from 'next-intl/middleware'
-import { locales, defaultLocale } from './i18n'
 
 export default createMiddleware({
-  locales,
-  defaultLocale,
+  locales: ['es', 'en'],
+  defaultLocale: 'es',
   localePrefix: 'as-needed',
 })
 
 export const config = {
-  matcher: ['/((?!api|_next|_vercel|.*\\..*).*)'],
+  matcher: ['/((?!_next|_vercel|.*\\..*).*)'],
 }
