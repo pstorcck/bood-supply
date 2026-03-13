d557d28a74b0a6ba806a4d985fe51ebe34681990
Paul Storck <paul@MacBook-Pro-de-Paul.local>
2026-03-12 18:06:59 -0600

    Add use client directive

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,4 +1,5 @@
 'use client'
+'use client'
 import Link from 'next/link'
 import { ArrowRight, CheckCircle2, DollarSign, ShieldCheck, HeadphonesIcon, Truck, MapPin, Phone, Mail } from 'lucide-react'
 
