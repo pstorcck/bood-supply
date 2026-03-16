5fb029fba42d85c152000ded21ec7b908ec8e640
Paul Storck <paul@MacBook-Pro-de-Paul.local>
2026-03-15 18:57:19 -0600

    Fix root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,11 @@
+import type { Metadata } from 'next'
+import './globals.css'
+
+export const metadata: Metadata = {
+  title: 'Bood Supply',
+  description: 'Distribuidor de suministros para restaurantes en Chicago',
+}
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return children
 }
