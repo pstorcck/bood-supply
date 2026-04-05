0476b963f102da57449449904f10eb49c9ca8765
Paul Storck <paul@MacBook-Pro-de-Paul.local>
2026-04-05 14:53:42 -0600

    feat: instalar Google Tag Manager GTM-5RB6229F

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -20,8 +20,16 @@ export default function LocaleLayout({ children }: { children: React.ReactNode }
     <html lang="es">
       <head>
         <link rel="icon" href="/logo.png" type="image/png" />
+        {/* Google Tag Manager */}
+        <script dangerouslySetInnerHTML={{ __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-5RB6229F');` }} />
       </head>
-      <body className={`${syne.variable} ${dmSans.variable}`}>{children}</body>
+      <body className={`${syne.variable} ${dmSans.variable}`}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5RB6229F" height="0" width="0" style={{display:'none',visibility:'hidden'}}></iframe>
+        </noscript>
+        {children}
+      </body>
     </html>
   )
 }
